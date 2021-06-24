e2c15d609c1b73c5abcbd393ccc6f137eb51496c
Hristo Georgiev <83413781+Hristo1Georgiev@users.noreply.github.com>
2021-06-24 18:18:01 +0200

    Add files via upload
    if / else statemants, js opdracht03

---
+++ JS03/script.js
@@ -0,0 +1,37 @@
+const age = 18;
+let custumerAge = 29;
+let custumerAge2 = 17;
+
+if (custumerAge >= age) {
+    console.log("Welcome!");
+}
+else {
+    console.log("You are not old enough to enter the bar, sorry!");
+}
+if (custumerAge2 >= age) {
+    console.log("Welcome!");
+}
+else {
+    console.log("You are not old enough to enter the bar, sorry!");
+}
+
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("welcome to the ladiesnight!");
+}
+else {
+    console.log("Hi, its a ladiesnight! ladies only!");
+}
+
+const driverStatus = "bob";
+let driverStatusTwee = "notBob"
+
+if (driverStatus == "bob") {
+    console.log("You are 'bob', drive safely to home!")
+
+}
+if (driverStatusTwee == "notBob") {
+    console.log("You are not 'bob', I will call you a taxi!")
+}
